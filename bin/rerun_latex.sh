44863fcfb99f937b5d0edf885457b290d686e11e
Fernando Pessoa <pessoa91@posteo.org>
2017-03-07 12:08:19 +0100

    improved latex

--- bin/rerun_latex.sh
+++ bin/rerun_latex.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 set -e
 
-logfile=$1
-PDF_LATEX=$2
-BIBTEX=$3
+logfile="$1"
+PDF_LATEX="$2"
+BIBTEX="$3"
 
 i=1
 cont=1
 while [[ $i -lt 5 ]] && [[ $cont -eq 1 ]] ; do
 	cont=0
 
-	if grep -q 'Citation .* undefined' $logfile || grep -q 'undefined references' $logfile; then
-		$BIBTEX
-		$PDF_LATEX
+	if grep -q 'Citation .* undefined' $logfile ; then
+		eval $BIBTEX
+		eval $PDF_LATEX
 		cont=1
 		((i+=2))
 	fi
 
-	if grep -q 'Rerun to get ' ${logfile} ; then
-		$PDF_LATEX
+	if grep -q 'Rerun to get ' ${logfile} || grep -q 'undefined references' $logfile; then
+		eval $PDF_LATEX
 		cont=1
 		((i++))
 	fi
